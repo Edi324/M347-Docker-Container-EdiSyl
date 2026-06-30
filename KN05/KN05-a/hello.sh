2b9bdad48c7e96d1b1d2f345521c7538699695e9
Edi324 <ediona.sylejmani@edu.tbz.ch>
2026-06-30 14:13:28 +0200

    did kns

---
+++ KN05/KN05-a/hello.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "--------------------------------"
+echo " KN05 Bind Mount Demo"
+echo " by Ediona Sylejmani"
+echo "--------------------------------"
+echo "Version 1 - edited from host"
+echo "Run inside container at: $(date)"
